d078cc5034e751a7de52347a6a78e2af8dcb7e59
jorge393 <albertotumadre2@gmail.com>
2022-08-17 14:55:48 -0300

    xdxd

--- db/01 SP.sql
+++ db/01 SP.sql
@@ -7,3 +7,24 @@ BEGIN
             VALUE (unNombre);
     SET unIdAutor = last_insert_id();
 END $$
+
+DELIMITER $$
+drop PROCEDURE if EXISTS altaEstudio;
+
+CREATE PROCEDURE altaEstudio (out unIdEstudio int, unNombre VARCHAR(45), unDomicilio VARCHAR(45))
+BEGIN 
+    INSERT INTO Estudio (nombre, dommicilio)
+            VALUE (unNombre, unDomicilio);
+    SET unIdEstudio = last_insert_id();
+END $$
+
+DELIMITER $$
+drop PROCEDURE if EXISTS altaAnime;
+
+CREATE PROCEDURE altaAnime (out unIdAnime int, unNombre VARCHAR(45), unEpisodios INT, unLanzamiento DATE, unEstado varchar(45) )
+BEGIN 
+    INSERT INTO Anime (nombre, episodios, lanzamiento, estado)
+            VALUE (unNombre, unEpisodios, unLanzamiento, unEstado);
+    SET unIdAnime = last_insert_id();
+END $$
+
