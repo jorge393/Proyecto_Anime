0d98ca1c2a880f41ea6e2e197f9b7d4517300fe8
jorge393 <albertotumadre2@gmail.com>
2022-09-22 15:48:21 -0300

    Estudio

---
+++ TPAnime.MVC/Controllers/EstudioController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using TPAnime.Core;
+
+namespace TPAnime.MVC.Controllers;
+public class EstudioController : Controller
+{
+    private readonly IAdo Ado;
+    public EstudioController(IAdo ado) => Ado = ado;
+
+    [HttpGet]
+    public IActionResult Index()
+        => View("Lista", Ado.obtenerAutores());
+
+    // [HttpGet]
+    // public IActionResult Detalle(int id)
+    // {
+    //     var categoria = Ado.obtenerAutores(id);
+    //     if (categoria is null)
+    //     {
+    //         return NotFound();
+    //     }
+    //     return View(categoria);
+    // }
+
+    [HttpGet]
+    public IActionResult FormAlta() => View();
+
+    [HttpPost]
+    public IActionResult FormAlta(Autor autor)
+    {
+        Ado.altaAutor(autor);
+        return View("Index", Ado.obtenerAutores());
+    }
+
+    // [HttpPost]
+    // public IActionResult Eliminar(int id)
+    // {
+    //     var categoria = Repositorio.GetCategoria(id);
+    //     if (categoria is null)
+    //     {
+    //         return NotFound();
+    //     }
+    //     Repositorio.EliminarCategoria(categoria);
+    //     return View("Index", Repositorio.Categorias);
+    // }
+
+    // [HttpGet]
+    // public IActionResult Modificar(int id)
+    // {
+    //     var categoria = Repositorio.GetCategoria(id);
+    //     if (categoria is null)
+    //     {
+    //         return NotFound();
+    //     }
+    //     return View(categoria);
+    // }
+
+//     [HttpPost]
+//     public IActionResult Modificar(Categoria categoria)
+//     {
+//         Repositorio.ActualizarCategoria(categoria);
+//         return View("Index", Repositorio.Categorias);
+//     }
+// }
